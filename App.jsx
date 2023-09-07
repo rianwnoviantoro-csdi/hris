be63c2994e30f7ce6d457ad9a5a947eba0777a14
rianwnoviantoro <tenkeylost@gmail.com>
2023-09-07 13:19:57 +0700

    add font and svg icons

--- App.jsx
+++ App.jsx
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useCallback } from "react";
+import {
+  useFonts,
+  Nunito_300Light,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+  Nunito_900Black,
+} from "@expo-google-fonts/nunito";
+import * as SplashScreen from "expo-splash-screen";
 
 import RootLayout from "./src/screens/_layout";
 
+SplashScreen.preventAutoHideAsync();
+
 const App = () => {
-  return <RootLayout />;
+  let [fontsLoaded] = useFonts({
+    nunito_300: Nunito_300Light,
+    nunito_400: Nunito_400Regular,
+    nunito_500: Nunito_500Medium,
+    nunito_600: Nunito_600SemiBold,
+    nunito_700: Nunito_700Bold,
+    nunito_800: Nunito_800ExtraBold,
+    nunito_900: Nunito_900Black,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <RootLayout onLayout={onLayoutRootView} />;
 };
 
 export default App;
---
+++ svgr.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  assets: ["./assets"],
+  replaceAttrValues: {
+    "#000": "currentColor",
+  },
+  // Add any other configuration options you need
+};
